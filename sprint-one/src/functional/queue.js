8b3aff104597b5b75687178853b0918af95d7ff0
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-05 20:32:12 -0400

    passed the functional tests for sprint-one

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,40 @@
 var Queue = function() {
+  // first one in, first one out
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // create keys mimicing indices
+  var last = 0;
+  var first = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // adding someone to the end of the line
+    storage[last] = value;
+    last++;
   };
 
   someInstance.dequeue = function() {
+    // store, remove, return first person in line
+    var remove = storage[first];
+    delete storage[first];
+    someInstance.size() && first++;
+    return remove;
   };
 
   someInstance.size = function() {
+    return last - first;
+    // // define a counter
+    // var counter = 0;
+    // // iterate through obj
+    // for (var item in storage) {
+    //   // increment counter
+    //   counter ++;
+    // }
+    // // return counter
+    // return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var last = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[last] = value;
+    last++;
   };
 
   someInstance.pop = function() {
+    var remove = storage[last - 1];
+    delete storage[last - 1];
+    someInstance.size() && last--;
+    return remove;
   };
 
   someInstance.size = function() {
+    return last;
   };
 
   return someInstance;
