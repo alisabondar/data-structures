641ed6b7aeb9af06109767f9df6dafae2c49d12c
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-06 13:18:12 -0400

    passed all sprint-one tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,22 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.start = 0;
+    this.end = 0;
   }
 
+  // add methods below
+  enqueue(value) {
+    this.storage[this.end++] = value;
+  }
+  dequeue() {
+    var remove = this.storage[this.start];
+    delete this.storage[this.start];
+    this.size() && this.start++;
+    return remove;
+  }
+  size() {
+    return this.end - this.start;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,19 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.end = 0;
   }
-
+  push(value) {
+    this.storage[this.end++] = value;
+  }
+  pop() {
+    var remove = this.storage[this.end - 1];
+    delete this.storage[this.end - 1];
+    this.size() && this.end--;
+    return remove;
+  }
+  size() {
+    return this.end;
+  }
 }
