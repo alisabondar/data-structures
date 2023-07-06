03edf68692a53cbd16f52078f5a2775671b797e9
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-06 11:55:06 -0400

    passed prototypal tests for queue.js and stack.js

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // utilize a protoype chain/lookup delegation
+  // use Object.create(queueMethods.prototype)
+  // create resulting obj with above
+  var obj = Object.create(queueMethods);
+  // create necessary variables - storage and start/end
+  obj.storage = {};
+  obj.start = 0;
+  obj.end = 0;
+  // return result
+  return obj;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.end++] = value;
+};
 
+queueMethods.dequeue = function() {
+  var remove = this.storage[this.start];
+  delete this.storage[this.start];
+  this.size() && this.start++;
+  return remove;
+};
+
+queueMethods.size = function() {
+  return this.end - this.start;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.storage = {};
+  obj.end = 0;
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.end++] = value;
+};
 
+stackMethods.pop = function() {
+  var remove = this.storage[this.end - 1];
+  delete this.storage[this.end - 1];
+  this.size() && this.end--;
+  return remove;
+};
+
+stackMethods.size = function() {
+  return this.end;
+};
