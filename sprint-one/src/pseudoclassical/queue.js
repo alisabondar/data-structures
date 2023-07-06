b33efb0f9c9b023a1cf9e647e7a984beeb58f21f
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-06 13:13:26 -0400

    passed pseudoclassical tests for queue.js & stack.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // using new for instantiation
+  // add supporting vars - storage and start/end
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.end++] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var remove = this.storage[this.start];
+  delete this.storage[this.start];
+  this.size() && this.start++;
+  return remove;
+};
+
+Queue.prototype.size = function() {
+  return this.end - this.start;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.end = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.end++] = value;
+};
+
+Stack.prototype.pop = function() {
+  var remove = this.storage[this.end - 1];
+  delete this.storage[this.end - 1];
+  this.size() && this.end--;
+  return remove;
+};
+
+Stack.prototype.size = function() {
+  return this.end;
+};
 
