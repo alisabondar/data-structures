41a1d2ac5c3a895a706bc9bf2a92c9ff602e98a2
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-06 10:27:27 -0400

    passed functional-shared tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // add variables here
+  var obj = {};
+  obj.storage = {};
+  obj.start = 0;
+  obj.end = 0;
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
+// var extend = function(obj1, obj2) {
+//   for (var item in obj2) {
+//     obj1[item] = obj2[item];
+//   }
+// };
+
+// add functions to method obj
+// functions that manipulate storage go here
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.end] = value;
+  this.end++;
+};
 
+queueMethods.dequeue = function() {
+  var remove = this.storage[this.start];
+  delete this.storage[this.start];
+  this.size() && this.start++;
+  return remove;
+};
+
+queueMethods.size = function() {
+  return this.end - this.start;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create resulting var
+  var obj = {};
+  // add helper properties - storage and end
+  obj.storage = {};
+  obj.end = 0;
+  // use extend to add methods
+  _.extend(obj, stackMethods);
+  // return resulting var
+  return obj;
 };
 
 var stackMethods = {};
 
+// create push, pop, size methods
+stackMethods.push = function(value) {
+  this.storage[this.end] = value;
+  this.end++;
+};
 
+stackMethods.pop = function() {
+  // store item to be removed
+  var remove = this.storage[this.end - 1];
+  delete this.storage[this.end - 1];
+  // check if size is truthy and decrement queue
+  this.size() && this.end--;
+  return remove;
+};
+
+stackMethods.size = function() {
+  return this.end;
+};
